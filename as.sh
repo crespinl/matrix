6402a3995e0eed865c31eb448c5cbe0c35045905
crespinl <louismj.crespin@gmail.com>
2023-02-12 21:23:04 +0100

    Updating copyright

--- as.sh
+++ as.sh
@@ -1,6 +1,6 @@
 echo "/*
 Matrix
-Copyright (C) 2022  Louis Crespin
+Copyright (C) 2022-2023  Louis Crespin
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
